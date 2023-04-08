79ac1b4e1fd75868ed0da748cd90ce3d896b0e4e
npranto <npranto@gmail.com>
2023-04-07 21:06:01 -0400

    uses microbundle to output cjs, esm and umd format

--- index.js
+++ index.js
@@ -1,101 +1,106 @@
+import axios from "axios";
+
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
 const AIRTABLE_TABLE_NAME = process.env.AIRTABLE_TABLE_NAME;
 
+export function airServiceLog() {
+  console.log("Airtable Service - Logs");
+}
+
 // Get all records from Airtable
-async function getAllRecords() {
+export async function getAllRecords() {
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
-  const response = await fetch(url, {
+  const response = await axios.get(url, {
     headers: {
       Authorization: `Bearer ${AIRTABLE_API_KEY}`,
     },
   });
-  if (response.ok) {
-    const data = await response.json();
-    return data.records;
+  if (response.status === 200) {
+    return response.data.records;
   } else {
-    throw new Error('Unable to get records from Airtable');
+    throw new Error("Unable to get records from Airtable");
   }
 }
 
 // Get a record by ID from Airtable
-async function getRecordById(recordId) {
+export async function getRecordById(recordId) {
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}/${recordId}`;
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-    },
-  });
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else if (response.status === 404) {
-    return null;
-  } else {
-    throw new Error('Unable to get record from Airtable');
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 404) {
+      return null;
+    } else {
+      throw new Error("Unable to get record from Airtable");
+    }
   }
 }
 
 // Create a new record in Airtable
-async function createRecord(data) {
+export async function createRecord(data) {
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-    },
-    body: JSON.stringify({ fields: data }),
-  });
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else {
-    throw new Error('Unable to create record in Airtable');
+  try {
+    const response = await axios.post(
+      url,
+      { fields: data },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Unable to create record in Airtable");
   }
 }
 
 // Update a record in Airtable
-async function updateRecord(recordId, data) {
+export async function updateRecord(recordId, data) {
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}/${recordId}`;
-  const response = await fetch(url, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-    },
-    body: JSON.stringify({ fields: data }),
-  });
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  } else if (response.status === 404) {
-    return null;
-  } else {
-    throw new Error('Unable to update record in Airtable');
+  try {
+    const response = await axios.patch(
+      url,
+      { fields: data },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 404) {
+      return null;
+    } else {
+      throw new Error("Unable to update record in Airtable");
+    }
   }
 }
 
 // Delete a record from Airtable
-async function deleteRecord(recordId) {
+export async function deleteRecord(recordId) {
   const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}/${recordId}`;
-  const response = await fetch(url, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-    },
-  });
-  if (response.ok) {
+  try {
+    const response = await axios.delete(url, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+      },
+    });
     return recordId;
-  } else if (response.status === 404) {
-    return null;
-  } else {
-    throw new Error('Unable to delete record from Airtable');
+  } catch (error) {
+    if (error.response.status === 404) {
+      return null;
+    } else {
+      throw new Error("Unable to delete record from Airtable");
+    }
   }
 }
-
-module.exports.getAllRecords = getAllRecords;
-module.exports.getRecordById = getRecordById;
-module.exports.createRecord = createRecord;
-module.exports.updateRecord = updateRecord;
-module.exports.deleteRecord = deleteRecord;
